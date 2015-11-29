79357e9a937bce8afd0954d1357c31db4730efe3
foxnaren <foxnaren@gmail.com>
2015-11-28 22:53:43 -0600

    chapter check for whenever

--- app/workers/spawn_check_latest_chapter.rb
+++ app/workers/spawn_check_latest_chapter.rb
@@ -5,15 +5,48 @@ require 'open-uri'
 class HourlyLatestChapterCheck
     include Sidekiq::Worker
 
-    1) Check status of Lightnovel
-	2) if there are no updates for 2 months switch status for hiatus or stopped
-		a) Switch to weekly checks
-	3) if no updates for 6 months switch to 6m Hiatus/Stopped or Complete status, do monthly status checks
+    def perform
 
-Manual check option for users.
-	
-	
+
+    	@lightnovel = Lightnovel.where.not()
+
+    	@lightnovel = Lightnovel.find_by(status: "Ongoing_Daily")
+    	@lightnovel.each do |lightnovel|
+    		case lightnovel.last_modified
+    		when "Ongoing_Daily"
+    			if 1.hour.ago > lightnovel.last_modified
+	    			CheckLatestChapter.perform_async(lightnovel.number_of_chapters, lightnovel.id)
+	    		end
+	    		if 1.week.ago > lightnovel.chapters.last.created_at
+	    			lightnovel.update_attributes(:last_modified => "Ongoing_Weekly")
+	    		end
+    		when "Ongoing_Weekly"
+	    		if 1.day.ago > lightnovel.last_modified
+	    			CheckLatestChapter.perform_async(lightnovel.number_of_chapters, lightnovel.id)
+	    		end
+	    		if 6.month.ago > lightnovel.chapters.last.created_at
+	    			lightnovel.update_attributes(:last_modified => "Ongoing_Monthly")
+	    		end
+	    	when "Ongoing_Monthly"
+	    		if 1.day.ago > lightnovel.last_modified
+	    			CheckLatestChapter.perform_async(lightnovel.number_of_chapters, lightnovel.id)
+	    		end
+	    		if 3.month.ago > lightnovel.chapters.last.created_at
+	    			lightnovel.update_attributes(:last_modified => "Hiatus_OR_Complete_3_Months")
+	    		end
+	    	when "Hiatus_OR_Complete_3_Months"
+	    		if 1.week.ago > lightnovel.last_modified
+	    			CheckLatestChapter.perform_async(lightnovel.number_of_chapters, lightnovel.id)
+	    		end
+
+    	end
+	end
+
+
 
 
 
 end
+
+
+
